3d08cf5bb0fc36b459f2bb34c62ec0b5ad10bc1d
MMM-yx <79987224+MMM-yx@users.noreply.github.com>
2021-04-13 21:27:38 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+function sayHello(){
+ var response = prompt("1+1=?");
+ alert("Hello " + response + ", have a good day!");
+ if (response=="2")
+   {alert("GOOD！")
+ else
+   {alert("Noooooooooooooo!")}
+}
