7e6ff627c9f843aacf86a7e251d5c9215076f992
MMM-yx <79987224+MMM-yx@users.noreply.github.com>
2021-04-14 15:33:57 +0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
 function sayHello(){
- var response = prompt("1+1=?");
- if (response=="2")
+ var response == prompt("1+1=?");
+ if (response =="2")
    {alert("GOOD！");}
  else
    {alert("Noooooooooooooo!");}
